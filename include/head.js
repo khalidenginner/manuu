5e0efa1f5a1d7f98d92e079247a715e6afa11598
khalidenginner <khalidenginner@outlook.com>
2024-10-14 04:39:09 -0700

    updates

---
+++ include/head.js
@@ -0,0 +1,19 @@
+document.write(`
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content>
+<meta name="keywords" content>
+
+<title>MANUU Help Desk</title>
+
+<link rel="icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/all-fontawesome.min.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+
+`);
---
+++ include/header.js
@@ -0,0 +1,215 @@
+document.write(`
+
+<div class="preloader">
+        <div class="loader-book">
+            <div class="loader-book-page"></div>
+            <div class="loader-book-page"></div>
+            <div class="loader-book-page"></div>
+        </div>
+    </div>
+<header class="header">
+
+        <div class="header-top">
+            <div class="container ps-0">
+                <div class="header-top-wrap">
+                    <div class="header-top-left">
+                        <div class="header-top-social">
+                            <span>Follow Us: </span>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-youtube"></i></a>
+                            <a href="#"><i class="fab fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+                    <div class="header-top-right">
+                        <div class="header-top-contact">
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="far fa-location-dot"></i> Darbhanga Bihar - 846001</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="far fa-envelopes"></i> <span>mhd@gmail.com</span></a>
+                                </li>
+                                <li>
+                                    <a href="tel:+911236547898"><i class="far fa-phone-volume"></i> +911236547898</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-navigation">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container position-relative">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/logo/logo.png" alt="logo">
+                    </a>
+                    <div class="mobile-menu-right">
+                        <div class="search-btn">
+                            <button type="button" class="nav-right-link search-box-outer"><i
+                                    class="far fa-search"></i></button>
+                        </div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="main_nav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link active" href="#" data-bs-toggle="dropdown">Home</a>
+                                
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link " href="#" data-bs-toggle="dropdown">Courses</a>
+                            </li>
+                            <li class="nav-item mega-menu dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Academics</a>
+                                <div class="dropdown-menu fade-down">
+                                    <div class="mega-content">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-12 col-sm-4 col-md-3">
+                                                    <h5>About Us</h5>
+                                                    <div class="menu-about">
+                                                        <a href="#" class="menu-about-logo"><img
+                                                                src="assets/img/logo/logo.png" alt></a>
+                                                        <p>We are many variations of passages available but the majority
+                                                            have suffered alteration in some form by injected humour
+                                                            words injected humour believable.</p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-sm-4 col-md-3">
+                                                    <h5>Undergraduate</h5>
+                                                    <ul class="mega-menu-item">
+                                                        <li><a class="dropdown-item" href="academic-single.html">Art And
+                                                                Design</a></li>
+                                                        <li><a class="dropdown-item" href="academic-single.html">Acting
+                                                                And Drama</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                href="academic-single.html">Accounting And Finance</a>
+                                                        </li>
+                                                        <li><a class="dropdown-item" href="academic-single.html">Biology
+                                                                And Conservation</a></li>
+                                                        <li><a class="dropdown-item" href="academic-single.html">Science
+                                                                And Engineering</a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="col-12 col-sm-4 col-md-3">
+                                                    <h5>Graduate Program</h5>
+                                                    <ul class="mega-menu-item">
+                                                        <li><a class="dropdown-item"
+                                                                href="academic-single.html">Software Systems</a></li>
+                                                        <li><a class="dropdown-item" href="academic-single.html">Human
+                                                                Resource</a></li>
+                                                        <li><a class="dropdown-item" href="academic-single.html">Health
+                                                                Administration</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                href="academic-single.html">Biochemistry</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                href="academic-single.html">Computer Science</a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="col-12 col-sm-12 col-md-3">
+                                                    <h5>Resources</h5>
+                                                    <ul class="mega-menu-item">
+                                                        <li><a class="dropdown-item" href="academic.html">Academics
+                                                                Department</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                href="academic-single.html">Academic Department
+                                                                Single</a></li>
+                                                        <li><a class="dropdown-item" href="faculty.html">Our Faculty</a>
+                                                        </li>
+                                                        <li><a class="dropdown-item" href="faculty-single.html">Faculty
+                                                                Single</a></li>
+                                                        <li><a class="dropdown-item" href="contact.html">Contact With
+                                                                Us</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link " href="#" data-bs-toggle="dropdown">About Us</a>
+                                
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Admissions</a>
+                                <ul class="dropdown-menu fade-down">
+                                    <li><a class="dropdown-item" href="how-to-apply.html">How To Apply</a></li>
+                                    <li><a class="dropdown-item" href="application-form.html">Application Form</a></li>
+                                    <li class="dropdown-submenu">
+                                        <a class="dropdown-item dropdown-toggle" href="#">Our Campus</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="campus-tour.html">Campus Tour</a></li>
+                                            <li><a class="dropdown-item" href="campus-life.html">Campus Life</a></li>
+                                            <li><a class="dropdown-item" href="notice-board.html">Notice Board</a></li>
+                                            <li><a class="dropdown-item" href="student-activities.html">Student
+                                                    Activities</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown-submenu">
+                                        <a class="dropdown-item dropdown-toggle" href="#">Our Facilities</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="facility.html">Facilities</a></li>
+                                            <li><a class="dropdown-item" href="facility-single.html">Facility Single</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown-submenu">
+                                        <a class="dropdown-item dropdown-toggle" href="#">Our Research</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="research.html">Research</a></li>
+                                            <li><a class="dropdown-item" href="research-single.html">Research Single</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown-submenu">
+                                        <a class="dropdown-item dropdown-toggle" href="#">Our Clubs</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="club.html">Our Clubs</a></li>
+                                            <li><a class="dropdown-item" href="club-single.html">Club Single</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a class="dropdown-item" href="tuition-fee.html">Tuition Fees</a></li>
+                                    <li><a class="dropdown-item" href="alumni.html">Alumni</a></li>
+                                    <li><a class="dropdown-item" href="scholarship.html">Scholarships</a></li>
+                                    <li><a class="dropdown-item" href="our-fund.html">Our Fund</a></li>
+                                    <li><a class="dropdown-item" href="athletics.html">Athletics</a></li>
+                                    <li><a class="dropdown-item" href="health-care.html">Health Care</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="blog.html" data-bs-toggle="dropdown">Blog</a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        </ul>
+                        <div class="nav-right">
+                            <div class="search-btn">
+                                <button type="button" class="nav-right-link search-box-outer"><i
+                                        class="far fa-search"></i></button>
+                            </div>
+                            <div class="nav-right-btn mt-2">
+                                <a href="application-form.html" class="theme-btn"><span
+                                        class="fal fa-pencil"></span>Student Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <div class="search-popup">
+        <button class="close-search"><span class="far fa-times"></span></button>
+        <form action="#">
+            <div class="form-group">
+                <input type="search" name="search-field" placeholder="Search Here..." required>
+                <button type="submit"><i class="far fa-search"></i></button>
+            </div>
+        </form>
+    </div>
+
+`);
